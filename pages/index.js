1269d18bcb77acd4707a8cf9a97155e97fa3bd20
Alessio Enrico Repetti <repeale@gmail.com>
2024-08-30 22:24:02 +0200

    chore: nextjs upgrade and static local resume

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 import styles from './index.module.css'
 
-const Home = ({cv}) => (
+import { resume } from '../src/resume'
+
+const Home = () => (
   <>
     <Head>
-      <meta name="description" content={cv.basics.name} />
-      <title>{cv.basics.name}</title>
+      <meta name="description" content={resume.basics.name} />
+      <title>{resume.basics.name}</title>
     </Head>
     <div className={styles.bg}>
       <div style={{margin: '20px'}}>
         <p>👋 Hello, I'm</p>
-        <h1>{cv.basics.name}</h1>
-        <p>{cv.basics.summary}</p>
+        <h1>{resume.basics.name}</h1>
+        <p>{resume.basics.summary}</p>
         <div className={styles.socialLinks}>
-          {cv.basics.profiles.slice(1).map((x, i) => (
+          {resume.basics.profiles.slice(1).map((x, i) => (
             <div key={i}>
               <a href={x.url} target="new">
                 {x.network}
@@ -26,15 +28,16 @@ const Home = ({cv}) => (
   </>
 )
 
-export async function getStaticProps() {
-  const res = await fetch('https://gitconnected.com/v1/portfolio/repeale')
-  const cv = await res.json()
+// export async function getStaticProps() {
+//   const res = await fetch('https://gitconnected.com/v1/portfolio/repeale')
+//   const resume = await res.json()
 
-  return {
-    props: {
-      cv,
-    },
-  }
-}
+
+//   return {
+//     props: {
+//       resume,
+//     },
+//   }
+// }
 
 export default Home
---
+++ src/resume.js
@@ -0,0 +1,279 @@
+export const resume = {
+    "basics": {
+        "name": "Alessio Enrico Repetti",
+        "label": "Engineering Lead @Moneyfarm",
+        "image": "https://avatars3.githubusercontent.com/u/9580458?v=4",
+        "email": "repeale@gmail.com",
+        "phone": "",
+        "url": null,
+        "summary": "10+ years of experience in Software Engineering",
+        "profiles": [
+            {
+                "network": "gitconnected",
+                "username": "repeale",
+                "url": "https://gitconnected.com/repeale"
+            },
+            {
+                "network": "GitHub",
+                "username": "repeale",
+                "url": "https://github.com/repeale"
+            },
+            {
+                "network": "LinkedIn",
+                "url": "https://www.linkedin.com/in/alessioenricorepetti",
+                "username": "alessioenricorepetti"
+            },
+            {
+                "network": "Twitter",
+                "url": "http://twitter.com/repeale87",
+                "username": "repeale87"
+            }
+        ],
+        "headline": "",
+        "blog": null,
+        "yearsOfExperience": 13,
+        "username": "repeale",
+        "locationAsString": "Milan, Italy",
+        "region": "Milan, Italy",
+        "karma": 36,
+        "id": "27184c85-46af-40e5-ae91-83c623350c5c",
+        "followers": 15,
+        "following": 38,
+        "picture": "https://avatars3.githubusercontent.com/u/9580458?v=4",
+        "website": null
+    },
+    "skills": [
+        {
+            "name": "Leadership",
+            "level": "",
+            "keywords": [
+                "Interpersonal"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        },
+        {
+            "name": "Management",
+            "level": "",
+            "keywords": [
+                "Interpersonal"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        },
+        {
+            "name": "TypeScript",
+            "level": "",
+            "keywords": [
+                "Coding",
+                "Languages"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        },
+        {
+            "name": "JavaScript",
+            "level": "",
+            "keywords": [
+                "Coding",
+                "Languages"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        },
+        {
+            "name": "React",
+            "level": "",
+            "keywords": [
+                "Coding"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        },
+        {
+            "name": "Node.js",
+            "level": "",
+            "keywords": [
+                "Coding"
+            ],
+            "rating": 0,
+            "yearsOfExperience": null
+        }
+    ],
+    "projects": [],
+    "work": [
+        {
+            "name": "Moneyfarm",
+            "location": "",
+            "description": "",
+            "position": "Tech Lead",
+            "url": "https://www.moneyfarm.com",
+            "startDate": "2019-09-01",
+            "endDate": "",
+            "summary": "",
+            "highlights": [
+                "Led a distributed Front-End team made of 4-7 engineers",
+                "Supervise Front-End architectural decisions",
+                "Mentoring holding technical one on one",
+                "Streamlined continuous deployment practices, reaching max 5 deployments per day",
+                "Streamlined testing practices",
+                "Streamlined TypeScript adoption",
+                "Part of hiring process. CV screening, first interview, technical interview."
+            ],
+            "isCurrentRole": true,
+            "start": {
+                "year": 2019,
+                "month": 9
+            },
+            "end": {},
+            "company": "Moneyfarm",
+            "website": "https://www.moneyfarm.com"
+        },
+        {
+            "name": "Moneyfarm",
+            "location": "",
+            "description": "",
+            "position": "Senior Software Engineer",
+            "url": "https://www.moneyfarm.com",
+            "startDate": "2019-01-01",
+            "endDate": "2022-01-01",
+            "summary": "",
+            "highlights": [],
+            "isCurrentRole": false,
+            "start": {
+                "year": 2019,
+                "month": 1
+            },
+            "end": {
+                "year": 2022,
+                "month": 1
+            },
+            "company": "Moneyfarm",
+            "website": "https://www.moneyfarm.com"
+        },
+        {
+            "name": "Contactlab",
+            "location": "",
+            "description": "",
+            "position": "Front-End Software Engineer",
+            "url": "https://contactlab.com",
+            "startDate": "2018-01-01",
+            "endDate": "2019-01-01",
+            "summary": "",
+            "highlights": [
+                "Developed, released and maintained the company flagship event driven marketing cloud automation project",
+                "Develop with agile (Scrum) methodology, test (TDD/BDD), maintain, single page web applications following company standards and best practices.",
+                "Worked closely to User Experience and User Interface designers"
+            ],
+            "isCurrentRole": false,
+            "start": {
+                "year": 2018,
+                "month": 1
+            },
+            "end": {
+                "year": 2019,
+                "month": 1
+            },
+            "company": "Contactlab",
+            "website": "https://contactlab.com"
+        },
+        {
+            "name": "Funambol",
+            "location": "",
+            "description": "",
+            "position": "Front-End Software Engineer",
+            "url": "https://funambol.com",
+            "startDate": "2013-07-01",
+            "endDate": "2018-01-01",
+            "summary": "",
+            "highlights": [
+                "Worked on https://zefiro.me white label personal cloud solution.",
+                "Develop with agile (Scrum) methodology, test (TDD/BDD), maintain, profile and improve responsive and mobile first web applications by evaluating business needs and analyzing requirements following company standards and best practices.",
+                "Tech analysis, product design, choice of technologies and developer tools, continuous improvements.",
+                "Respond to feedbacks given by Technical Project Managers, work closely with Back End Engineers, User Experience designer, Product Owners.",
+                "Develop, test, and maintain proprietary web framework.",
+                "REST Api definition for client-server communication.",
+                "Streamlined Node.js adoption."
+            ],
+            "isCurrentRole": false,
+            "start": {
+                "year": 2013,
+                "month": 7
+            },
+            "end": {
+                "year": 2018,
+                "month": 1
+            },
+            "company": "Funambol",
+            "website": "https://funambol.com"
+        },
+        {
+            "name": "Self Employed",
+            "location": "",
+            "description": "",
+            "position": "Freelance Web Developer",
+            "url": "",
+            "startDate": "2009-01-01",
+            "endDate": "2013-01-01",
+            "summary": "",
+            "highlights": [],
+            "isCurrentRole": false,
+            "start": {
+                "year": 2009,
+                "month": null
+            },
+            "end": {
+                "year": 2013,
+                "month": null
+            },
+            "company": "Self Employed",
+            "website": ""
+        }
+    ],
+    "publications": [],
+    "education": [
+        {
+            "institution": "Università degli Studi di Pavia",
+            "url": "",
+            "area": "Computer Engineering",
+            "studyType": "Bachelor's degree",
+            "startDate": "",
+            "endDate": "",
+            "score": "",
+            "courses": [],
+            "description": "",
+            "activities": "",
+            "start": {
+                "year": null,
+                "month": null
+            },
+            "end": {
+                "year": null,
+                "month": null
+            },
+            "website": "",
+            "gpa": ""
+        }
+    ],
+    "volunteer": [],
+    "awards": [],
+    "languages": [
+        {
+            "language": "English",
+            "fluency": "Professional proficiency"
+        },
+        {
+            "language": "Italian",
+            "fluency": "Native"
+        }
+    ],
+    "interests": [],
+    "references": [],
+    "certificates": [],
+    "meta": {
+        "note": "This is a combined version of the old and new JSON Resume API to provide maximum compatibility. You will notice duplicated values in different fields.",
+        "canonical": "https://raw.githubusercontent.com/jsonresume/resume-schema/master/resume.json",
+        "version": "v1.0.0",
+        "lastModified": "2020-12-29T15:53:00"
+    }
+}
